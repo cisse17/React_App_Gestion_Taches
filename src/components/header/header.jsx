564a127d8f08a9b155b992f2000bb39d161a48fd
cisse17 <119404406+cisse17@users.noreply.github.com>
2024-11-03 21:50:59 +0100

    first commit, configuration projet et création de sous dossier

---
+++ src/components/header/header.jsx
@@ -0,0 +1,25 @@
+// pour afficher  l'en-tete de 
+import styles from './header.module.css';
+import {reactLogo} from "../../assets/react.svg"
+
+const Header = () => {
+  return (
+    <div  className={styles.container}>
+      <div className={styles.titleContainer}>
+        <img src={reactLogo} alt='logo' width={50} height={50} />
+        <div>
+            <h1>TaskFlow</h1>
+            <div className='color-gray'>
+                <code>Eliminez le chaoc, suivez le flux.</code>
+            </div>
+        </div>
+      </div>
+
+      <div className="color-primary">
+            v.1.0
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/taskContainer.jsx
@@ -0,0 +1,14 @@
+
+// import Header from "./header/header";
+import Header from "./header";
+
+// ce composant est utilisé pour afficher l'intégralité de la fonctionnalité de tache
+export const TaskContainer = () => {
+  return (
+    <main>
+       <Header/>
+    </main>
+  );
+};
+
+
